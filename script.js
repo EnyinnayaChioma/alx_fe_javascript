21c7c7343966a85c77918fa61bec56452dc9d055
Eninnaya Chioma <enyinnayachioma738@gmail.com>
2025-01-30 15:23:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// An array to store quotes and their categories
+const quotes = [
+    { text: "The only way to do great work is to love what you do.", category: "Motivation" },
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "In the middle of difficulty lies opportunity.", category: "Opportunity" }
+];
